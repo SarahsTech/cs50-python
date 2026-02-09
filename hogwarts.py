4cbae0dd4a1d7f96862f7ee3123d1501aa06a79f
Sarah Beckwith <purposewithsarah@gmail.com>
2026-02-09 18:59:39 +0100

    Added comments

--- hogwarts.py
+++ hogwarts.py
@@ -1,6 +1,13 @@
+# imports a csv file with student names and houses
 import csv
 
+# open the csv file named students.csv
+# newline="" prevents blank lines from appearing between rows on Windows
 with open("students.csv", newline='') as file:
+    # create a DictReader object which reads each raw as a dictionary
     reader = csv.DictReader(file)
+    # loop through each row (dictionary) in the csv file
     for row in reader:
+        # row['name'] gets the value under the name column
+        # row['house'] gets the value under the house column
         print(f"{row['name']}: {row['house']}")
